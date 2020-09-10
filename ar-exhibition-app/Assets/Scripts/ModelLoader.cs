07025e5b1579ab4aa110ceb7df8f571607cba65f
TheCanadians <vschnoor@mc-kiel.de>
2020-09-10 12:12:48 +0200

    Added Methods to download and show images as sprites Added functionality to automatically scale images to fit screen size better

--- ar-exhibition-app/Assets/Scripts/ModelLoader.cs
+++ ar-exhibition-app/Assets/Scripts/ModelLoader.cs
@@ -10,14 +10,19 @@ public class ModelLoader : MonoBehaviour
 {
 
     GameObject wrapper;
+    GameObject spriteWrapper;
+    Sprite sprite;
+    Texture2D tex;
     string filePath;
 
     [SerializeField] float modelDimension = 3f;
+    [SerializeField] float imageDimension = 3f;
 
     // Start is called before the first frame update
     void Start()
     {
         filePath = $"{Application.persistentDataPath}/Files/";
+        tex = new Texture2D(600, 600);
     }
 
     public void DownloadFile(string url)
@@ -43,12 +48,33 @@ public class ModelLoader : MonoBehaviour
         }));
     }
 
+    public void DownloadTexture(string url)
+    {
+        string path = GetFilePath(url);
+        CreateSpriteWrapper();
+
+        StartCoroutine(GetImageRequest(url, (response) =>
+        {
+            sprite = response;
+            spriteWrapper.GetComponent<SpriteRenderer>().sprite = sprite;
+            ResizeImage(spriteWrapper);
+        }));
+        
+    }
+
+
     string GetFilePath(string url)
     {
         string[] pieces = url.Split('/');
         string fileName = pieces[pieces.Length - 1];
 
         return $"{filePath}{fileName}";
+    }
+
+    void LoadImage(string path)
+    {
+        
+        CreateSpriteWrapper();
     }
 
     void LoadModel(string path)
@@ -60,6 +86,15 @@ public class ModelLoader : MonoBehaviour
         Resize(wrapper);
     }
 
+    void CreateSpriteWrapper()
+    {
+        spriteWrapper = new GameObject
+        {
+            name = "Image"
+        };
+        spriteWrapper.AddComponent<SpriteRenderer>();
+    }
+
     void CreateWrapper()
     {
         wrapper = new GameObject
@@ -68,13 +103,45 @@ public class ModelLoader : MonoBehaviour
         };
     }
 
+    void ResizeImage(GameObject image)
+    {
+        SpriteRenderer sr = image.GetComponent<SpriteRenderer>();
+
+        float max = Mathf.Max(Mathf.Max(sr.bounds.size.x, sr.bounds.size.y), sr.bounds.size.z);
+        float scalingFactor = imageDimension / max;
+        image.transform.localScale *= scalingFactor;
+    }
+
     void Resize(GameObject model)
     {
         MeshRenderer renderer = model.GetComponentInChildren<MeshRenderer>();
 
         float max = Mathf.Max(Mathf.Max(renderer.bounds.size.x, renderer.bounds.size.y), renderer.bounds.size.z);
-        float scalingFactor = max * modelDimension;
+        float scalingFactor = modelDimension / max;
         model.transform.localScale *= scalingFactor;
+    }
+
+    IEnumerator GetImageRequest(string url, System.Action<Sprite> callback)
+    {
+        using (var req = UnityWebRequestTexture.GetTexture(url))
+        {
+            yield return req.SendWebRequest();
+            
+            if (req.isNetworkError || req.isHttpError)
+            {
+                Debug.Log(req.error);
+            }
+            else
+            {
+                if (req.isDone)
+                {
+                    var texture = DownloadHandlerTexture.GetContent(req);
+                    var rect = new Rect(0, 0, texture.width, texture.height);
+                    var sprite = Sprite.Create(texture, rect, new Vector2(0.5f, 0.5f));
+                    callback(sprite);
+                }
+            }
+        }
     }
 
     IEnumerator GetFileRequest(string url, Action<UnityWebRequest> callback)
