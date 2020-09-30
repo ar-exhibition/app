7e5904be937c24c2e38f322908a15bb0b5339110
Marlon Lückert <m.lueckert@me.com>
2020-09-30 23:10:08 +0200

    added intro scene

---
+++ ar-exhibition-app/Assets/Scripts/IntroUIManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEngine.SceneManagement;
+
+public class IntroUIManager : MonoBehaviour
+{
+
+    private UIDocument _UIDocument;
+    private VisualElement _root;
+    private Button _startExhibitionButton;
+    private Button _createExhibitionButton;
+
+
+    void Awake()
+    {   
+        _UIDocument = GetComponent<UIDocument>();
+        _root = _UIDocument.rootVisualElement;
+        _startExhibitionButton = _root.Q<Button>("startExhibitionButton");
+        _createExhibitionButton = _root.Q<Button>("createExhibitionButton");
+    }
+
+    void OnEnable() {
+        _startExhibitionButton.clicked += OnStartExhibition;
+        _createExhibitionButton.clicked += OnCreateExhibition;
+    }
+    void OnDisable() {
+        _startExhibitionButton.clicked -= OnStartExhibition;
+        _createExhibitionButton.clicked -= OnCreateExhibition;
+    }
+
+    void OnStartExhibition() {
+
+    }
+
+    void OnCreateExhibition() {
+        SceneManager.LoadScene("CuratorScene", LoadSceneMode.Single);
+    }
+}
