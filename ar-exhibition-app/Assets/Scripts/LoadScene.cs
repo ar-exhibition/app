02daccf7dcde2fbabcc4196bc5f4996914eb11b7
TheCanadians <vschnoor@mc-kiel.de>
2020-10-15 14:28:21 +0200

    Added Scene Loading GameObject that doesn't get destroyed by loading new scene

--- ar-exhibition-app/Assets/Scripts/LoadScene.cs
+++ ar-exhibition-app/Assets/Scripts/LoadScene.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
 
+
 public class LoadScene : MonoBehaviour
 {
 
@@ -12,6 +13,8 @@ public class LoadScene : MonoBehaviour
     MarkerManager _markerManager;
     List<Scene> _sceneList;
 
+    SceneInfo _sceneInfo;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +22,10 @@ public class LoadScene : MonoBehaviour
         _referenceImage = GetComponent<ARTrackedImage>().referenceImage;
         _markerManager = GameObject.FindObjectOfType<MarkerManager>();
         _sceneList = _markerManager.GetSceneList();
+
+        _sceneInfo = GameObject.FindObjectOfType<SceneInfo>();
+
+        FindWorldLink(_referenceImage.name);
     }
 
     void FindWorldLink (string imageName)
@@ -29,6 +36,8 @@ public class LoadScene : MonoBehaviour
             {
                 Debug.Log("Found Name: " + scene.marker.name);
                 Debug.Log("Corresponding worldMapLink: " + scene.worldMapLink);
+                _sceneInfo.SetScene(scene);
+                _sceneInfo.LoadScene(_sceneInfo.GetSceneType());
             }
         }
     }
